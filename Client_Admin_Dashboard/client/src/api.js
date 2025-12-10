448c2b62226677fe8305587b29a6c69928c9d6bb
Aditya-Patel-04 <adityatirole04@gmail.com>
2025-12-10 16:52:40 +0530

    Update api.js

--- Client_Admin_Dashboard/client/src/api.js
+++ Client_Admin_Dashboard/client/src/api.js
@@ -1,5 +1,6 @@
 /** @format */
 
 import axios from "axios";
-export default axios.create({ baseURL: "https://techcrop-server-o7si.onrender.com" });
+export default axios.create({ baseURL: "https://techcrop-server-o7si.onrender.com/api" });
+
 
